eb242b40aef86cefcf819071ed141c01bc28f15c
Hunter Thomas <archangelq@gmail.com>
2013-12-30 18:06:42 -0800

    Initial template with bower integration

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AngularRailsTemplate::Application.config.secret_token = '6f35c2ef32286c6aa1a3bbf4d4c8cb9e146142b1399e5eebcce514e4103badadf5780a7266e7e0765591b0a8e056bc89ef308c4607c4c872aee52295537caa27'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AngularRailsTemplate::Application.config.session_store :cookie_store, key: '_angular-rails-template_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# AngularRailsTemplate::Application.config.session_store :active_record_store
