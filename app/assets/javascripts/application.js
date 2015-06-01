45d8c86cab37d201b9012e6a88b22cfb3a6901a8
Hunter Hunnie <scrapcupcake@gmal.com>
2015-05-31 21:29:41 -0700

    Why did this not commit.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery_ujs
 //= require angular/angular
 //= require angular-rails-templates
+//= require angular-rails-templates
 //= require angular-ui-router/release/angular-ui-router
 //= require angular-bootstrap/ui-bootstrap-tpls
 //= require angularjs/rails/resource
